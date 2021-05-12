8ef7d57b061869aa03a2346e93922c22c04fe9e4
Krzysztof Mędrzycki <aiwenari@gmail.com>
2021-05-12 11:54:14 +0200

    Argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use argh::FromArgs;
+
+/// Publish package as a GIT tag
+#[derive(FromArgs)]
+struct Args {
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = argh::from_env();
 }
