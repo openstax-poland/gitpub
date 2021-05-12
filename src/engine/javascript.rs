be8e6961f8fee0550dde5f1704f5019a13507ce4
Krzysztof Mędrzycki <aiwenari@gmail.com>
2021-05-12 15:29:52 +0200

    Initial engine for JavaScript

---
+++ src/engine/javascript.rs
@@ -0,0 +1,127 @@
+use anyhow::{Context, Result, bail};
+use json::{JsonValue, object::Object};
+use std::{fs, path::PathBuf, process::Command};
+
+use super::Engine;
+
+/// Select appropriate engine for a JavaScript project
+pub fn select(path: PathBuf) -> Result<Box<dyn Engine>> {
+    let mut lock = path.parent().unwrap().join("yarn.lock");
+
+    let has_yarn_lock = lock.exists();
+
+    lock.pop();
+    lock.push("package-lock.json");
+
+    let has_npm_lock = lock.exists();
+
+    // yarn.lock exists, use yarn
+    if has_yarn_lock || !has_npm_lock {
+        let ver = Command::new("yarn").arg("--version").output()?;
+
+        if ver.stdout.starts_with(b"1.") {
+            return JavaScript::new_in(Yarn, path);
+        }
+
+        if ver.stdout.starts_with(b"2.") {
+            return JavaScript::new_in(Yarn2, path);
+        }
+
+        if has_yarn_lock {
+            bail!("found unsupported version of Yarn")
+        }
+    }
+
+    JavaScript::new_in(Npm, path)
+}
+
+/// Engine for JavaScript projects
+pub struct JavaScript<C> {
+    /// Contents of `package.json`
+    pkg: Object,
+    /// Package name
+    name: String,
+    /// Package version
+    version: String,
+    /// Client selected
+    _client: C,
+}
+
+impl<C: Client + 'static> JavaScript<C> {
+    fn new(_client: C) -> Result<Box<dyn Engine>> {
+        Self::new_in(_client, std::env::current_dir()?.join("package.json"))
+    }
+
+    fn new_in(_client: C, path: PathBuf) -> Result<Box<dyn Engine>> {
+        let data = fs::read_to_string(&path).context("could not read package.json")?;
+
+        let pkg = match json::parse(&data).context("invalid package.json")? {
+            JsonValue::Object(pkg) => pkg,
+            _ => bail!("package.json is not a JSON object"),
+        };
+
+        let name = pkg.get("name").context("package has no name")?
+            .as_str().context("package name is not a string")?
+            .to_string();
+        let version = pkg.get("version").context("package has no version")?
+            .as_str().context("package version is not a string")?
+            .to_string();
+
+        Ok(Box::new(JavaScript { pkg, name, version, _client }))
+    }
+}
+
+impl JavaScript<Npm> {
+    pub fn npm() -> Result<Box<dyn Engine>> {
+        JavaScript::new(Npm)
+    }
+}
+
+impl JavaScript<Yarn> {
+    pub fn yarn() -> Result<Box<dyn Engine>> {
+        JavaScript::new(Yarn)
+    }
+}
+
+impl JavaScript<Yarn2> {
+    pub fn yarn2() -> Result<Box<dyn Engine>> {
+        JavaScript::new(Yarn2)
+    }
+}
+
+impl<C: Client> Engine for JavaScript<C> {
+    fn name(&self) -> String {
+        format!("JavaScript / {}", C::NAME)
+    }
+
+    fn pkg_name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn pkg_version(&self) -> String {
+        self.version.clone()
+    }
+}
+
+/// npm client
+pub trait Client {
+    const NAME: &'static str;
+}
+
+pub struct Npm;
+
+impl Client for Npm {
+    const NAME: &'static str = "Npm";
+}
+
+pub struct Yarn;
+
+impl Client for Yarn {
+    const NAME: &'static str = "Yarn";
+}
+
+pub struct Yarn2;
+
+impl Client for Yarn2 {
+    const NAME: &'static str = "Yarn 2";
+}
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,5 +1,7 @@
 use anyhow::{Result, bail};
 
+mod javascript;
+
 pub trait Engine {
     /// Engine name
     fn name(&self) -> String;
@@ -13,12 +15,22 @@ pub trait Engine {
 
 /// Select the most appropriate engine
 pub fn select() -> Result<Box<dyn Engine>> {
+    let mut path = std::env::current_dir()?;
+
+    path.push("package.json");
+    if path.exists() {
+        return javascript::select(path);
+    }
+
     bail!("could not select engine, please specify it via --engine")
 }
 
 /// Select engine by name
 pub fn by_name(name: &str) -> Result<Box<dyn Engine>> {
     match name {
+        "npm" => javascript::JavaScript::npm(),
+        "yarn" => javascript::JavaScript::yarn(),
+        "yarn2" | "yarn-2" | "yarn-berry" => javascript::JavaScript::yarn2(),
         _ => bail!("no engine named {}", name),
     }
 }
